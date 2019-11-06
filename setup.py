29af1488d56f0c1d81368649f87430c1f22ff0bf
Pearson Henri <pearsonhenri@gmail.com>
2019-11-06 16:25:47 -0300

    update repo link when publishing

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Inspetor Antifraud library for Python.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/inspetor/inspetor-python",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
